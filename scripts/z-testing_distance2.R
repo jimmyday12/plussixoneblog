98c620afecac2541c70bd3a88a4cfb1eabf7b8f2
James Day <jamesthomasday@gmail.com>
2018-04-04 15:44:41 +1000

    testing distance

--- scripts/z-testing_distance2.R
+++ scripts/z-testing_distance2.R
@@ -11,7 +11,19 @@ grounds <- wiki_html %>%
   html_nodes("table") %>%
   html_table(fill = T) %>%
   .[1:3] %>%
-  map_dfr(select, Ground, City)
+  map_dfr(select, Ground:Capacity) 
+
+names(grounds) <- make.names(names(grounds))
+
+grounds <- grounds %>%
+  mutate(State = ifelse(is.na(State), State.territory, State),
+         in.world.cities = City %in% maps::world.cities$name)
+
+capitals <- data.frame(
+  State = c("Victoria", "Tasmania", "New South Wales", "Queensland", "South Australia", "Western Australia", "Australian Capital Territory", "Northern Territory"),
+  City = c("Melbourne", "Hobart", "Sydney", "Brisbane", "Adelaide", "Perth", "Canberra", "Darwin")
+)
+
 
 # Step 2: Write datatable with team and home city
 
