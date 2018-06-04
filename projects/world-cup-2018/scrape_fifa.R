99e8d434af3df5fe7611dd09abd763e5a0858b20
James Day <jamesthomasday@gmail.com>
2018-06-04 13:48:28 +1000

    Updated with purrr and downloaded data

--- projects/world-cup-2018/scrape_fifa.R
+++ projects/world-cup-2018/scrape_fifa.R
@@ -1,70 +1,63 @@
-
 library(pacman)
-pacman::p_load(XML, ggplot2, reshape2, quantreg, plyr, data.table)
+pacman::p_load(tidyverse, lubridate, rvest, tictoc)
+tic()
 
-metaURL <-"http://www.fifa.com/worldranking/rankingtable"
-metaParse <- htmlParse(metaURL)
-
-########################################
-
-getRankingTableURL <- function(
-  gender="m",
-  rank=238, #238 is most recent as of May2014 - counts down to older
-  confederation=0, #0 gets all teams
-  page=1 #counts up from one for more and more teams
-){
-  base="http://www.fifa.com/worldranking/rankingtable"
-  tail="_ranking_table.html"
+# Create function to generate URL
+get_ranking_table_URL <- function(rank, confederation, gender = "m", page = 1){
+  base <- "http://www.fifa.com/worldranking/rankingtable"
+  tail <- "_ranking_table.html"
   return(paste0(base,
-                "/gender=",gender,
-                "/rank=",rank,
-                "/confederation=",confederation,
-                "/page=",page,
+                "/gender=", gender,
+                "/rank=", rank,
+                "/confederation=", confederation,
+                "/page=", page,
                 "/",
                 tail)
   ) 
 }
-getRankingTableURL(confederation=23913)
 
-#######################################
-### Get the nodes I want via XPATH query
-#
-start_rank = 286 #may want to replace with scraped number
-end_rank = 57 #57 corresponds to Jan99, when point method was revised
-conf_ids = c(23913,23914,23915,23916,25998,27275)
+# Generate URLS ----------------------------------------------------------------
+# Specify parameters
+start_rank = 286 # the last update (as of Jun 5th, 2018)
+end_rank = 57 # corresponds to Jan99, when point method was revised
+conf_ids = c(23913, 23914, 23915, 23916, 25998, 27275)
 
+rank <- rep(start_rank:end_rank, each = length(conf_ids))
+conf_vec <- rep(conf_ids, times = length(start_rank:end_rank))
 
-pointDF <- data.frame()
-for(rank in start_rank:end_rank){
-  for(confi in conf_ids){
-    tableURL <- getRankingTableURL(rank=rank,confederation=confi)
-    tableParse <- htmlParse(tableURL)
-    
-    #Get date of the table
-    dtString <- xmlValue(getNodeSet(tableParse,"//div[@class='rnkwrap rnkdate']")[[1]][['div']])
-    dt <- as.Date(paste0("1 ",gsub("^\\s*","",dtString)),"%d %B %Y")
-    
-    #Get table data
-    tableList <- getNodeSet(tableParse,"//table[@id='tbl_rankingTable']")
-    tableM <-  t(
-      sapply(
-        tableList[[1]][['tbody']]['tr'],
-        function(x){
-          tdList <- x['td']
-          c(
-            #rank=xmlValue(tdList[[2]]),
-            name=gsub("^\\s*","",xmlValue(tdList[[4]])),
-            #abr=gsub("'","",strsplit(xmlGetAttr(x,"onclick"),",")[[1]][2]),
-            points=gsub("^\\s*","",xmlValue(tdList[[5]]))
-          )
-        }
-      )
-    )
-    
-    #Turn table matrix in to data.frame
-    pointDF <- rbind(pointDF,data.frame(date=dt,confid=confi,tableM, stringsAsFactors=F))
-  }
-}
+urls <- rank %>%
+  map2_chr(conf_vec, ~ get_ranking_table_URL(rank = .x, confederation = .y))
 
+# Read each URL and download the HTML
+# Found this code to do a progress bar - kind of ugly but nice to know where we are at
+pb <- progress_estimated(length(urls))
+htmls <- urls %>%
+  map(~{
+    pb$tick()$print()
+    read_html(.x)
+    })
 
-write.csv(pointDF,"fifa_rank_history.csv",row.names=F)
+# Read date
+dates <- htmls %>% 
+  map(~html_nodes(.x, ".lb")) %>%
+  map(html_text) %>%
+  map_chr(~str_remove(.x, "Last Updated ")) %>%
+  map(dmy) 
+
+# Read Tables
+tables <- htmls %>% 
+  map(~html_nodes(.x, "#tbl_rankingTable")) %>%
+  map(html_table) %>%
+  map(as.data.frame)
+
+# Add date, conf id and make into one dataframe
+dat <- tables %>%
+  map2(dates, ~ .x %>% mutate(Date = .y)) %>%
+  map2(conf_vec, ~ .x %>% mutate(Conference_id = .y)) %>%
+  reduce(bind_rows) %>%
+  select(Date, Team, Pts, Conference_id) %>%
+  arrange(Date, desc(Pts))
+
+# Write out
+write_csv(dat, here::here("projects", "world-cup-2018", "fifa_rank_history.csv"))
+toc()
