f6b5531c372f2659d3820daa7c2a1a9d52a759dc
James Day <17557361+jimmyday12@users.noreply.github.com>
2020-08-09 16:09:17 +1000

    adding check to skip simulating if no new data is found

--- scripts/weekly_data_process/weekly_data_process.R
+++ scripts/weekly_data_process/weekly_data_process.R
@@ -1,6 +1,6 @@
 # Script to run weekly updating of data, ratings simulations and predictions. Data should be saved into github for us by blog.
 ptm <- proc.time()
-
+message("Starting Script")
 # Preamble ----------------------------------------------------------------
 # Load libraries
 library(fitzRoy)
@@ -38,169 +38,184 @@ dat <- get_data(season,
                 grand_final_bug = grand_final_bug, 
                 fixture_bug = fixture_bug)
 
-print(proc.time() - ptm)
-message("ELO Loaded")
-
-# Data Cleaning -----------------------------------------------------------
-# Bind together and fix stadiums
-dat$game_dat <- bind_rows(dat$results, dat$fixture) %>%
-  mutate(Game = row_number()) %>%
-  ungroup() %>%
-  mutate(Venue = stringr::str_trim(Venue) %>% venue_fix()) %>%
-  mutate(Round = Round.Number)
-
-# ELO Prep
-last_n_games <- 100
-dat$game_dat <- elo_prep_calculations(dat$game_dat, 
-                                      states = dat$states, 
-                                      last_n_games = last_n_games)
-
-
-# Get results
-dat$results <- dat$game_dat %>%
-  filter(!is.na(Home.Points))
-
-# Get fixture
-dat$fixture <- dat$game_dat %>%
-  filter(is.na(Home.Points))
-
-# COVID Fix 
-covid_seas <- last(dat$fixture$Round) < 23
-
-if(covid_seas) dat$fixture <- fix_covid_season(dat$fixture)
 
 print(proc.time() - ptm)
-message("Data Cleaned")
+message("Data Loaded")
 
-# Run ELO calculation -----------------------------------------------------
-elo_dat <- run_elo(dat$results, carryOver = carryOver,
-        B = B, e = e, d = d, h = h)
+old_results <- read_rds(here::here("data_files", "raw-data", "AFLM.rds"))
+if(last(old_results$results$Home.Team) == last(dat$results$Home.Team) &
+   last(old_results$results$Away.Team) == last(dat$results$Away.Team) & 
+   last(old_results$results$Date) == last(dat$results$Date)) {
+  new_data <- FALSE
+  message("No New Data Found")
+} else {
+  new_data <- TRUE
+  message("New Data Found")
+}
 
-dat$results <- elo_dat$results
-
-# Message
-print(proc.time() - ptm)
-message("ELO Run")
-
-# Predictions -------------------------------------------------------------
-# Do predictions
-dat$predictions <- do_elo_predictions(dat$fixture, elo_dat$elo.data)
-
-# Message
-print(proc.time() - ptm)
-message("Predictions Done")
-
-# Simulation --------------------------------------------------------------
-message("Simulating")
-sim_dat <- list()
-
-# do sims
-sim_dat <- append(sim_dat, 
-                   do_sims(sim_num, dat$results, dat$fixture, elo_dat$elo.data))
-
-# combine
-sim_dat$sim_data_all <- combine_sim_dat(sim_dat$sim_dat)
-
-if (new_season) {
-  season = last(dat$results$Season) + 1
-  round = 0 
+if(new_data) {
+  
+  # Data Cleaning -----------------------------------------------------------
+  # Bind together and fix stadiums
+  dat$game_dat <- bind_rows(dat$results, dat$fixture) %>%
+    mutate(Game = row_number()) %>%
+    ungroup() %>%
+    mutate(Venue = stringr::str_trim(Venue) %>% venue_fix()) %>%
+    mutate(Round = Round.Number)
+  
+  # ELO Prep
+  last_n_games <- 100
+  dat$game_dat <- elo_prep_calculations(dat$game_dat, 
+                                        states = dat$states, 
+                                        last_n_games = last_n_games)
+  
+  
+  # Get results
+  dat$results <- dat$game_dat %>%
+    filter(!is.na(Home.Points))
+  
+  # Get fixture
+  dat$fixture <- dat$game_dat %>%
+    filter(is.na(Home.Points))
+  
+  # COVID Fix 
+  covid_seas <- last(dat$fixture$Round) < 23
+  
+  if(covid_seas) dat$fixture <- fix_covid_season(dat$fixture)
+  
+  print(proc.time() - ptm)
+  message("Data Cleaned")
+  
+  # Run ELO calculation -----------------------------------------------------
+  elo_dat <- run_elo(dat$results, carryOver = carryOver,
+                     B = B, e = e, d = d, h = h)
+  
+  dat$results <- elo_dat$results
+  
+  # Message
+  print(proc.time() - ptm)
+  message("ELO Run")
+  
+  # Predictions -------------------------------------------------------------
+  # Do predictions
+  dat$predictions <- do_elo_predictions(dat$fixture, elo_dat$elo.data)
+  
+  # Message
+  print(proc.time() - ptm)
+  message("Predictions Done")
+  
+  # Simulation --------------------------------------------------------------
+  message("Simulating")
+  sim_dat <- list()
+  
+  # do sims
+  sim_dat <- append(sim_dat, 
+                    do_sims(sim_num, dat$results, dat$fixture, elo_dat$elo.data))
+  
+  # combine
+  sim_dat$sim_data_all <- combine_sim_dat(sim_dat$sim_dat)
+  
+  if (new_season) {
+    season = last(dat$results$Season) + 1
+    round = 0 
   } else {
     season = last(dat$results$Season)
     round = last(dat$results$Round)
   }
-
-# summarise
-sim_dat$sim_data_summary  <- calculate_sim_perc(sim_dat$sim_data_all, 
-                                                season, 
-                                                round, sim_num)
-
-# Combine these simulations with previous ones for plotting
-# Load old sims
-past_sims <- read_rds(here::here("data_files", "raw-data", "AFLM_sims.rds")) 
-combine_past_sims(sim_dat$sim_data_summary, round, season, past_sims)
-
-# Print to console
-sim_dat$sim_data_summary %>% 
-  filter(Round == round) %>%
-  tail()
-
-sim_dat$simCount <- count_sims(sim_dat$sim_data_all, 
-                               sim_dat$sim_data_summary,
-                               sim_num, 
-                               season, 
-                               round, 
-                               past_sims)
-
-rm(past_sims)
-
-# MEssage
-print(proc.time() - ptm)
-message("Sims done")
-
-# Finals Sims -------------------------------------------------------------
-# source finals sims
-message("Doing Finals Sims")
-last_round <- last(dat$fixture$Round.Number)
-finals_dat <- do_finals_sims(sim_data_all = sim_dat$sim_data_all, 
-               game_dat = dat$game_dat, 
-               sim_num = sim_num, 
-               elo.data = elo_dat$elo.data,
-               sim_elo_perterbed = sim_dat$sim_elo_perterbed ,
-               last_round = last_round)
-
-finals_dat <- combine_finals_sims(final_game = finals_dat$final_game,
-                    sim_data_summary = sim_dat$sim_data_summary, 
-                    results = dat$results, 
-                    elo.data = elo_dat$elo.data)
-
-print(proc.time() - ptm)
-message("Finals Sims done")
-
-# Save Data ---------------------------------------------------------------
-message("Saving data")
-# Create list
-aflm_data <- list(
-  results = dat$results,
-  elo.data = elo_dat$elo.data, 
-  elo = elo_dat$elo, 
-  predictions = dat$predictions)
-
-aflm_sims <- list(
-  sim_data_summary = sim_dat$sim_data_summary,
-  sim_data_all = sim_dat$sim_data_all,
-  simCount = sim_dat$simCount
-)
-
-rm(sim_dat)
-rm(dat)
-
-# Save
-write_rds(finals_dat, path = here::here("data_files", "raw-data", "AFLM_finals_sims.rds"), compress = "bz")
-write_rds(aflm_data, path = here::here("data_files", "raw-data", "AFLM.rds"), compress = "bz")
-write_rds(aflm_sims, path = here::here("data_files", "raw-data", "AFLM_sims.rds"), compress = "bz")
-
-# Predictions
-predictions_csv <- aflm_data$predictions %>%
-  select(Season, Date, Home.Team, Away.Team, Probability, Prediction)
-
-write_csv(aflm_data$predictions, path = here::here("data_files", "raw-data", "predictions.csv"))
-write_csv(predictions_csv, path = here::here("data_files", "raw-data", "predictions_new.csv"))
-
-# Writing csv
-write_csv(aflm_sims$sim_data_summary, path = here::here("data_files", "raw-data", "AFLM_sims_summary.csv"))
-write_csv(aflm_sims$simCount, path = here::here("data_files", "raw-data", "AFLM_sims_positions.csv"))
-write_csv(aflm_data$elo, path = here::here("data_files", "raw-data","AFLM_elo.csv"))
-
-# Message
-print(proc.time() - ptm)
-message("Data saved")
-
-# Touch files ------------------------------------------------------------------
-blogdown:::touch_file(here::here("content", "page", "aflm-ratings-and-simulations.Rmd"))
-blogdown:::touch_file(here::here("content", "page", "aflm-current-tips.Rmd"))
-blogdown:::touch_file(here::here("content", "page", "aflm-predictions.Rmd"))
-blogdown:::touch_file(here::here("content", "page", "aflm-games.Rmd"))
-
+  
+  # summarise
+  sim_dat$sim_data_summary  <- calculate_sim_perc(sim_dat$sim_data_all, 
+                                                  season, 
+                                                  round, sim_num)
+  
+  # Combine these simulations with previous ones for plotting
+  # Load old sims
+  past_sims <- read_rds(here::here("data_files", "raw-data", "AFLM_sims.rds")) 
+  combine_past_sims(sim_dat$sim_data_summary, round, season, past_sims)
+  
+  # Print to console
+  sim_dat$sim_data_summary %>% 
+    filter(Round == round) %>%
+    tail()
+  
+  sim_dat$simCount <- count_sims(sim_dat$sim_data_all, 
+                                 sim_dat$sim_data_summary,
+                                 sim_num, 
+                                 season, 
+                                 round, 
+                                 past_sims)
+  
+  rm(past_sims)
+  
+  # MEssage
+  print(proc.time() - ptm)
+  message("Sims done")
+  
+  # Finals Sims -------------------------------------------------------------
+  # source finals sims
+  message("Doing Finals Sims")
+  last_round <- last(dat$fixture$Round.Number)
+  finals_dat <- do_finals_sims(sim_data_all = sim_dat$sim_data_all, 
+                               game_dat = dat$game_dat, 
+                               sim_num = sim_num, 
+                               elo.data = elo_dat$elo.data,
+                               sim_elo_perterbed = sim_dat$sim_elo_perterbed ,
+                               last_round = last_round)
+  
+  finals_dat <- combine_finals_sims(final_game = finals_dat$final_game,
+                                    sim_data_summary = sim_dat$sim_data_summary, 
+                                    results = dat$results, 
+                                    elo.data = elo_dat$elo.data)
+  
+  print(proc.time() - ptm)
+  message("Finals Sims done")
+  
+  # Save Data ---------------------------------------------------------------
+  message("Saving data")
+  # Create list
+  aflm_data <- list(
+    results = dat$results,
+    elo.data = elo_dat$elo.data, 
+    elo = elo_dat$elo, 
+    predictions = dat$predictions)
+  
+  aflm_sims <- list(
+    sim_data_summary = sim_dat$sim_data_summary,
+    sim_data_all = sim_dat$sim_data_all,
+    simCount = sim_dat$simCount
+  )
+  
+  rm(sim_dat)
+  rm(dat)
+  
+  # Save
+  write_rds(finals_dat, path = here::here("data_files", "raw-data", "AFLM_finals_sims.rds"), compress = "bz")
+  write_rds(aflm_data, path = here::here("data_files", "raw-data", "AFLM.rds"), compress = "bz")
+  write_rds(aflm_sims, path = here::here("data_files", "raw-data", "AFLM_sims.rds"), compress = "bz")
+  
+  # Predictions
+  predictions_csv <- aflm_data$predictions %>%
+    select(Season, Date, Home.Team, Away.Team, Probability, Prediction)
+  
+  write_csv(aflm_data$predictions, path = here::here("data_files", "raw-data", "predictions.csv"))
+  write_csv(predictions_csv, path = here::here("data_files", "raw-data", "predictions_new.csv"))
+  
+  # Writing csv
+  write_csv(aflm_sims$sim_data_summary, path = here::here("data_files", "raw-data", "AFLM_sims_summary.csv"))
+  write_csv(aflm_sims$simCount, path = here::here("data_files", "raw-data", "AFLM_sims_positions.csv"))
+  write_csv(aflm_data$elo, path = here::here("data_files", "raw-data","AFLM_elo.csv"))
+  
+  # Message
+  print(proc.time() - ptm)
+  message("Data saved")
+  
+  # Touch files ------------------------------------------------------------------
+  blogdown:::touch_file(here::here("content", "page", "aflm-ratings-and-simulations.Rmd"))
+  blogdown:::touch_file(here::here("content", "page", "aflm-current-tips.Rmd"))
+  blogdown:::touch_file(here::here("content", "page", "aflm-predictions.Rmd"))
+  blogdown:::touch_file(here::here("content", "page", "aflm-games.Rmd"))
+  
+}
 
 print(proc.time() - ptm)
 message("Finished!")
